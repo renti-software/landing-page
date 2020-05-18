3a29ea0253e5ea912f198bb29c69180333378827
Tom1k <tomascosta@ua.pt>
2020-05-18 21:10:02 +0100

    Updated team roles in static page

--- renti/src/components/Home/About/About.js
+++ renti/src/components/Home/About/About.js
@@ -24,7 +24,19 @@ function About() {
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-4">
+                        <div class="col-md-3">
+                            <div class="fh5co-person text-center to-animate">
+                                <figure><img src={team04} alt="Image" /></figure>
+                                <h3>Tomás Costa</h3>
+                                <span class="fh5co-position">Team Manager</span>
+                                <ul class="social social-circle">
+                                    <li><a href=""><i class="icon-globe"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/in/tomascostax/"><i class="icon-linkedin"></i></a></li>
+                                    <li><a href="https://github.com/TomasCostaK"><i class="icon-github"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
                             <div class="fh5co-person text-center to-animate">
                                 <figure><img src={team01} alt="Image" /></figure>
                                 <h3>João Marques</h3>
@@ -36,64 +48,27 @@ function About() {
                                 </ul>
                             </div>
                         </div>
-                        <div class="col-md-4">
-                            <div class="fh5co-person text-center to-animate">
-                                <figure><img src={team02} alt="Image" /></figure>
-                                <h3>João Vasconcelos</h3>
-                                <span class="fh5co-position">Team Manager</span>
-                                <ul class="social social-circle">
-                                    <li><a href="https://jmnmv12.github.io/AboutMePT"><i class="icon-globe"></i></a></li>
-                                    <li><a href="https://www.linkedin.com/in/joão-vasconcelos/"><i class="icon-linkedin"></i></a></li>
-                                    <li><a href="https://github.com/jmnmv12"><i class="icon-github"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="fh5co-person text-center to-animate">
-                                <figure><img src={team03} alt="Image" /></figure>
-                                <h3>Tiago Mendes</h3>
-                                <span class="fh5co-position">Software Architect</span>
-                                <ul class="social social-circle">
-                                    <li><a href="https://tiagocmendes.github.io/"><i class="icon-globe"></i></a></li>
-                                    <li><a href="https://pt.linkedin.com/in/tiagocmendes"><i class="icon-linkedin"></i></a></li>
-                                    <li><a href="https://github.com/tiagocmendes"><i class="icon-github"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row" style={{marginTop: "100px"}}>
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <div class="fh5co-person text-center to-animate">
                                 <figure><img src={team04} alt="Image" /></figure>
-                                <h3>Tomás Costa</h3>
-                                <span class="fh5co-position">Lead Developer</span>
+                                <h3>Francisco Jesus</h3>
+                                <span class="fh5co-position">Product Owner</span>
                                 <ul class="social social-circle">
-                                    <li><a href="https://tomascosta.netlify.com/"><i class="icon-globe"></i></a></li>
+                                    <li><a href=""><i class="icon-globe"></i></a></li>
                                     <li><a href="https://www.linkedin.com/in/tomascostax/"><i class="icon-linkedin"></i></a></li>
                                     <li><a href="https://github.com/TomasCostaK"><i class="icon-github"></i></a></li>
                                 </ul>
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <div class="fh5co-person text-center to-animate">
-                                <figure><img src={team05} alt="Image" /></figure>
-                                <h3>Vasco Ramos</h3>
-                                <span class="fh5co-position">Team Manager</span>
+                                <figure><img src={team04} alt="Image" /></figure>
+                                <h3>Miguel Matos</h3>
+                                <span class="fh5co-position">QA Engineer</span>
                                 <ul class="social social-circle">
-                                    <li><a href="https://vascoalramos.me/"><i class="icon-globe"></i></a></li>
-                                    <li><a href="https://linkedin.com/in/vascoalramos"><i class="icon-linkedin"></i></a></li>
-                                    <li><a href="https://github.com/vascoalramos"><i class="icon-github"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="fh5co-person text-center to-animate">
-                                <figure><img src={team06} alt="Image" /></figure>
-                                <h3>Prof. Dr. Carlos Costa</h3>
-                                <span class="fh5co-position">Advisor</span>
-                                <ul class="social social-circle">
-                                    <li><a href="http://sweet.ua.pt/carlos.costa/default.htm"><i class="icon-globe"></i></a></li>
-                                    <li><a href="https://gitlab.com/nao.consigo.nome"><i class="icon-github"></i></a></li>
+                                    <li><a href=""><i class="icon-globe"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/in/tomascostax/"><i class="icon-linkedin"></i></a></li>
+                                    <li><a href="https://github.com/TomasCostaK"><i class="icon-github"></i></a></li>
                                 </ul>
                             </div>
                         </div>
